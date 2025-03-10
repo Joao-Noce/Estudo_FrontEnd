8c93e63fe4070471aea5c7e1cfa5ad94340378d7
Joao-Nocee <joao.noce@sptech.school>
2025-03-10 16:13:52 -0300

    Edições

--- index/index.js
+++ index/index.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("#form");
-const userList = document.querySelector("#users_list");
+const userList = document.querySelector("#users_list_cards");
+const ulList = document.querySelector("#users_list_ul");
+const divList = document.querySelector("#users_list_div");
 
 const cadastrar = (verbo, id) => {
 
@@ -11,12 +13,12 @@ const cadastrar = (verbo, id) => {
         nome = input_nome.value;
         email = input_email.value;
         password = input_password.value;
-        url = "http://localhost:8080/users"
+        url = "http://localhost:5000/users"
     } else {
         nome = input_nome_editar.value;
         email = input_email_editar.value;
         password = input_password_editar.value;
-        url = `http://localhost:8080/users/${id}`
+        url = `http://localhost:5000/users/${id}`
     }
     if (nome != '' && email != '' && password != '') {
         const newUser = { nome, email, password }
@@ -35,14 +37,16 @@ const cadastrar = (verbo, id) => {
 }
 
 const getUsers = () => {
-    fetch("http://localhost:8080/users")
+    fetch("http://localhost:5000/users")
         .then((res) => {
             if (res.ok) return res.json();
             else console.error(res);
         })
         .then((dados) => {
             console.table(dados);
-            showUsers(dados);
+            // showUsers(dados);
+            // lista(dados);
+            listaDiv(dados);
         })
 }
 
@@ -51,6 +55,7 @@ const showUsers = (dados) => {
     userList.innerHTML = '';
     dados.forEach(user => {
         const newForm = document.createElement('form');
+        const newSpanId = document.createElement('span');
         const newInputNome = document.createElement('input');
         const newInputEmail = document.createElement('input');
         const newInputPassword = document.createElement('input');
@@ -59,6 +64,7 @@ const showUsers = (dados) => {
         const newButtonDeletar = document.createElement('button');
         const newButtonAdicionar = document.createElement('button');
 
+        newSpanId.textContent = user.id;
         newButtonDiv.className = 'form_container_button';
         newButtonAdicionar.textContent = 'Adicionar';
 
@@ -101,6 +107,7 @@ const showUsers = (dados) => {
             }
         })
 
+        newForm.appendChild(newSpanId);
         newForm.appendChild(newInputNome);
         newForm.appendChild(newInputEmail);
         newForm.appendChild(newInputPassword);
@@ -147,11 +154,92 @@ const criarNovo = () => {
 }
 
 const deletar = (id) => {
-    fetch(`http://localhost:8080/users/${id}`, {
+    fetch(`http://localhost:5000/users/${id}`, {
         method: 'DELETE'
     })
         .then((res) => console.log(res))
         .catch((error) => console.error(error))
+}
+
+const lista = (dados) => {
+    ulList.innerHTML = '';
+    dados.forEach(user => {
+        const newLine = document.createElement('li');
+        newLine.textContent = `Nome: ${user.nome} | Email: ${user.email}`;
+
+        ulList.appendChild(newLine);
+        ulList.innerHTML += `<br>`;
+    });
+}
+
+const listaDiv = (dados) => {
+    divList.innerHTML = '';
+    dados.forEach(user => {
+        const newDiv = document.createElement('div');
+        const newSpanNome = document.createElement('span');
+        const newSpanEmail = document.createElement('span');
+        const newSpanSenha = document.createElement('span');
+        const newEditar = document.createElement('button');
+        const newDeletar = document.createElement('button');
+
+        newEditar.textContent = 'Editar';
+        newDeletar.textContent = 'Deletar';
+
+        newSpanNome.textContent = user.nome + ' - ';
+        newSpanEmail.textContent = user.email + ' - ';
+        newSpanSenha.textContent = user.password;
+
+        newDeletar.addEventListener('click', (event) => {
+            deletar(user.id);
+            divList.removeChild(newDiv);
+        })
+        newEditar.addEventListener('click', (event) => {
+            newSpanNome.id = 'span_nome';
+            newSpanEmail.id = 'span_email';
+            newSpanSenha.id = 'span_senha';
+
+            const novoNome = prompt("Digite o novo Nome: ", newSpanNome.textContent);
+            const novoEmail = prompt("Digite o novo Email: ", newSpanEmail.textContent);
+            const novoSenha = prompt("Digite o novo Senha: ", newSpanSenha.textContent);
+            newSpanNome.textContent = novoNome;
+            newSpanEmail.textContent = novoEmail;
+            newSpanSenha.textContent = novoSenha;
+            editar(user.id);
+
+        })
+        newDiv.appendChild(newSpanNome);
+        newDiv.appendChild(newSpanEmail);
+        newDiv.appendChild(newSpanSenha);
+        newDiv.appendChild(newEditar);
+        newDiv.appendChild(newDeletar);
+        divList.appendChild(newDiv);
+    });
+}
+
+
+
+function editar(id) {
+
+    const nome = document.getElementById('span_nome').textContent;
+    const email = document.getElementById('span_email').textContent;
+    const password = document.getElementById('span_senha').textContent;
+
+    fetch(`http://localhost:5000/users/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            nome,
+            email,
+            password
+        })
+
+    }).then((res) => {
+        console.log(res);
+        getUsers();
+
+    }).catch((error) => {
+        console.log(error);
+    })
+
 }
 
 window.onload = getUsers();
